1d5433ab44bda1206c6338cb0b4ae0b100a9c95a
admin <admin@itcst.cn>
2019-05-19 20:10:49 +0800

    java  再修改4

--- gitTest/src/com/itheima/gitTest.java
+++ gitTest/src/com/itheima/gitTest.java
@@ -8,18 +8,5 @@ public class gitTest {
         System.out.println("时间事解药！");
     }
 
-    public  void  Test(){
 
-        System.out.println("时间咋样！");
-    }
-
-    public  void  Test2(){
-
-        System.out.println("欧克.......！");
-///已交代吗测试i
-    }
-    public  void  Test3(){
-        System.out.println("3333333333333333333333");
-        //赵小波
-    }
 }
