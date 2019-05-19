f65d94c2b7efad1bc284fe19d576100ef55ff8ca
admin <admin@itcst.cn>
2019-05-19 20:23:21 +0800

    java  再修改6

--- gitTest/src/com/itheima/gitTest.java
+++ gitTest/src/com/itheima/gitTest.java
@@ -3,12 +3,9 @@ package com.itheima;
 public class gitTest {
 
     public static void main(String[] args) {
-
         System.out.println("时间事解药！");
         System.out.println("时间事解药！");
         System.out.println("时间事解药！");
 
     }
-
-
 }
